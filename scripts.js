a72af3f89fd991183f8221ba3c780207d9b4649e
TranMinhQuyet <44421023+caigiha1@users.noreply.github.com>
2022-04-12 15:30:00 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ A team only win if it has at least double the average scor of the other team. Ot
 3. Create a function 'Checkwinner' that takes the average score of eachteam as parameters('avgDolphins' and 'avgKoalas') and then log the winner */
 
 const calcAverage = (score1, score2 ,score3) => (score1 + score2 + score3) / 3;
-console.log(calcAverage(1,2,3));
+// console.log(calcAverage(1,2,3));
 
 let scoreDolphins = calcAverage(900, 60 , 100);
 let scoreKoalas = calcAverage(80,23,45);
@@ -40,3 +40,36 @@ const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 
 console.log(bills, tips, totals);
+
+
+///// small challenge
+
+
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    birthYear: 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven'],
+    hasDriversLicense: false,
+
+    caclAge: function() {
+        this.age = 2022 - this.birthYear;
+        return this.age;
+    },
+
+    hasDriver: function() {
+        if(this.hasDriversLicense){
+            return this.hasDriver = 'a';
+        }else {
+            return this.hasDriver = 'no';
+        }
+    }
+}
+
+console.log(`${jonas.firstName} is a ${jonas.caclAge()}-year old teacher, and he has ${jonas.hasDriver()} driver's license`);
+
+
+
+
+
