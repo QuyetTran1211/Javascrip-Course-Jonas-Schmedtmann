f1cdfb065e428556f110b5d2dfe12c6776d5be1c
TranMinhQuyet <44421023+caigiha1@users.noreply.github.com>
2022-04-14 17:15:00 +0700

    Update some line scripts.js

--- part-5-guess-number/script.js
+++ part-5-guess-number/script.js
@@ -1,1 +1,72 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = '?';
+console.log(secretNumber);
+let score = 20;
+let highScore = 0;
+
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess);
+
+    if(!guess) {
+        document.querySelector('.message').textContent = '⛔ No number!'; 
+    } 
+    // Correct answer
+    else if(guess === secretNumber){
+        document.querySelector('.message').textContent = '🎉 Correct Number!'; 
+        document.querySelector('.number').textContent = secretNumber;
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+
+        if(score > highScore){
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore;
+        }
+
+    } 
+    // Lower answer
+    else if (guess < secretNumber){
+        if(score > 1){
+            document.querySelector('.message').textContent = '📉 Too Low';
+            score--;
+            document.querySelector('.score').textContent = score;
+        }else {
+            document.querySelector('.message').textContent = '￣へ￣ You Loose';
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+    // Higher answer
+    else if (guess > secretNumber) {
+        if(score > 1){
+            document.querySelector('.message').textContent = '📈 Too High';
+            score--;
+            document.querySelector('.score').textContent = score;
+        }else {
+            document.querySelector('.message').textContent = '￣へ￣ You Loose';
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+});
+
+/// Challenge #1
+// Again button
+document.querySelector('.again').addEventListener('click', function () {
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.score').textContent = 20;
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('body').style.backgroundColor = '#222';
+});
+
