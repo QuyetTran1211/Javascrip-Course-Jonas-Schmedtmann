420f62092da8b918b4ef15502920de9447a60e4e
TranMinhQuyet <44421023+caigiha1@users.noreply.github.com>
2022-05-19 09:28:44 +0700

    complete coding challenge #2

--- part-14-OOP/script.js
+++ part-14-OOP/script.js
@@ -20,6 +20,8 @@
 //   console.log(2037 - this.birthYear);
 // };
 
+// Coding Challange #1
+
 // const Car = function (make, speed) {
 //   this.make = make;
 //   this.speed = speed;
@@ -49,16 +51,125 @@
 
 // Class declaration
 class PersonCl {
-  constructor(firstName, birthYear) {
-    this.firstName = firstName;
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
     this.birthYear = birthYear;
   }
+
+  // Instance Methods
+  // Methods will be added to .prototype property
 
   calcAge() {
     console.log(2037 - this.birthYear);
   }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  // Set a property that already exist
+  set fullName(name) {
+    console.log(name);
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
 }
 
-const jessica = new PersonCl('Jessica', 1996);
+const jessica = new PersonCl('Jessica Davis', 1996);
 console.log(jessica);
 jessica.calcAge();
+console.log(jessica.age);
+
+// 1. Classes are Not hoisted
+// 2. Classes are first-class citizens
+// 3. Classes are executed in stric mode
+
+const walter = new PersonCl('Walter White', 1956);
+
+// const account = {
+//   owner: 'Jonas',
+//   movement: [200, 530, 120, 300],
+
+//   get lastest() {
+//     return this.movement.slice(-1).pop();
+//   },
+
+//   set lastest(mov) {
+//     this.movement.push(mov);
+//   },
+// };
+
+// console.log(account.lastest);
+
+// account.lastest = 50;
+
+// console.log(account.movement);
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+steven.init('Steven', 1986);
+steven.calcAge();
+
+// Coding Challenge #2
+
+/* 
+1. Re-create challenge 1, but this time using an ES6 class;
+2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6);
+3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value, by multiplying the input by 1.6);
+4. Create a new car and experiment with the accelerate and brake methods, and with the getter and setter.
+
+DATA CAR 1: 'Ford' going at 120 km/h
+
+GOOD LUCK 😀
+*/
+
+class Car {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} going at ${this.speed} km/h`);
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} going at ${this.speed} km/h`);
+  }
+
+  get speedUS() {
+    return `${this.make} going at ${this.speed / 1.6} mi/h`;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+const ford = new Car('Ford', 120);
+console.log(ford.speedUS);
+ford.accelerate();
+ford.accelerate();
+ford.brake();
+ford.speedUS = 50;
+console.log(ford);
